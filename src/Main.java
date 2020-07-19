6d41f83535a1c7de3c154764fa211f459acf99bd
Użytkownik <uytkownik@kursGIT.pl>
2020-07-19 12:23:45 +0200

    zad14

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// Napisz program, który pobierze od użytkownika dwie małe litery alfabetu łacińskiego (typ char)  i  wyliczy,  ile  znaków  stoi  w  alfabecie  pomiędzy  podanymi  literami.
+        // Podpowiedź – skorzystaj z tablicy kodów ASCII i traktuj znaki jak liczby int
+
+
+
+    }
+}
